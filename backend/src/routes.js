79265bc1588a66f41118c7489c589293d6911ae0
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-03-27 15:56:10 -0300

    Banco de dados funcional metodos http de criaçao e listagem de ongs feita

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const crypto = require('crypto');
+const connections = require('./database/connections');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
-    console.log(body);
 
-    return response.json({
-        evento: 'Semana Omnistack 11',
-        aluno: 'Diego F. Fernandes'
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connections('ongs').select('*');
+    return response.json(ongs);
+})
+
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connections('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
     });
+
+    return response.json({id});
 });
 
 module.exports = routes;
