a5128e942e1d58f09b25ed832e163914495d9a35
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-03-28 20:22:09 -0300

    Backend pronto

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,25 @@ const connections = require('../database/connections');
 
 module.exports = {
     async index(request, response){
-        const incidents = await connections('incidents').select('*');
+        const {page = 1} = request.query;
+
+        const [count] = await connections('incidents').count();
+
+        const incidents = await connections('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+        
         return response.json(incidents);
     },
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
-
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/profile', ProfileController.index);
 
