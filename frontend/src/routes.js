5b9fcffb330e95c7294c0efbe604408e787413a1
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-03-30 19:13:28 -0300

    Estilizaçao de pagina de casos do usuario concluida

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/profile" component={Profile}/>
             </Switch>
         </BrowserRouter>
     )
