735113b7bbd98edd76934e038d11d48f3e54b83e
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-03 09:37:19 -0300

    Função de mandar msgs para o whatsapp pronta

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
 
@@ -27,7 +27,7 @@ export default function Detail() {
     }
 
     function sendWhatsapp(){
-        
+        Linking.openURL(`whatsapp://send?phone=5527996480747&text=${message}`);
     }
 
 
@@ -58,7 +58,7 @@ export default function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
