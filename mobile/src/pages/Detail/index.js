50de3dc155e16db3ece3ad6a3d33bff3746fbdf1
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-03 08:58:45 -0300

    Função de enviar emails feita

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import {Feather} from '@expo/vector-icons'
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
+
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 
-export default function Detail(){
+export default function Detail() {
+    const navigation = useNavigation();
+    const message = 'bla bla bla bla';
+
+    function navigateBack() {
+        navigation.goBack(null);
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: cadelinha fudida',
+            recipients: ['devpm21@gmail.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp(){
+        
+    }
+
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name='arrow-left' size={28} color='#E02041' />
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
-                <Text style={[styles.incidentProperty, {marginTop: 0}]}>Ong:</Text>
+                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>Ong:</Text>
                 <Text style={styles.incidentValue}>APAD</Text>
-                
+
                 <Text style={styles.incidentProperty}>Caso:</Text>
                 <Text style={styles.incidentValue}>Cadelinha fudida</Text>
-                
+
                 <Text style={styles.incidentProperty}>Valor:</Text>
                 <Text style={styles.incidentValue}>R$: 120,00</Text>
             </View>
@@ -31,15 +54,15 @@ export default function Detail(){
             <View style={styles.contactBox}>
                 <Text style={styles.heroTitle}>Salve o dia!</Text>
                 <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
-            
+
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
-                
+
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={() => { }}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
